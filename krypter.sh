fef14853fdf25a687ca61e3f74b60b88a62c486c
silence <abaird0893@gmail.com>
2023-08-09 01:50:35 +0100

    v1.0 of krypter

--- krypter.sh
+++ krypter.sh
@@ -1,76 +1,141 @@
 #! /usr/bin/env bash
-#
-# script for encryption / decryption of folders and files
-#       encrypt | take in file or directory >>> tar.gz >>> encrypt >>> delete file/directory & tar.gz
-#       decrypt | take in encrypted file >>> decrypt to tar.gz >>> unarchive to file/directory >>> delete tar.gz and encrypted file
-#
-# NOTES 
-#       tar czf <name.tar.gz> --directory=<name>/ .
+# =======================================================================================================================
+#   HEADER
+# =======================================================================================================================
+# 
+#   SYNOPSIS
+#       krypter -der file
 #       
-#       mkdir <dirName>
-#       tar xzf <name.tar.gz> -C <dirName>
-#       
-#       gpg --cipher-algo AES256 --symmetric <name.tar.gz>
+#   DESCRIPTION
+#       A script that takes to either encrypt or decrypt a file
+#       In encryption mode it will convert a file or directory to a tar.gz archive, then encrypt that archive using the AES256 algorithm using a key you supply when prompted
+#       In decryption mode the process is reversed. 
+#       In both cases the .tar.gz file is temporary
 #
-#       gpg --output <name.tar.gz> --decrypt <name.tar.gz.gpg>
+#   OPTIONS
+#       -d,     decrypt file
+#       -e,     encypt file/ directory 
+#       -r,     delete the original file on success
 #
+#   EXAMPLES
+#       krypter -e sensitive-file.txt
+#       krypter -er ./nudes/
+#       krypter -d video-tapes.gpg
 #
-${1?: You must select whether to encrypt or decrypt}
-${2?: You must supply a file or folder}
+#   Version=1.0
+#   TODO's ------------------------------------------------------------
+#       add checks to alert if tar or gpg are not installed
 #
-#   TODO add better argument checking and error handling
-#   TODO add help output
-#   TODO add ability to parse file / directory input from outside local directory >>> basename?
-#    
+#   -------------------------------------------------------------------
+# =======================================================================================================================
+#   END OF HEADER
+# =======================================================================================================================
+
+# FUNCTIONS #############################################################################################
+HELP() {
+    echo -e '\033[1mKRYPTER HELP\033[0m'
+    echo -e 'Encrypt or decrypt a file / directory with gpg \n'
+    echo 'Syntax: krypter [OPTIONS...] [FILE]'
+    echo -e 'enter secret key when prompted \n'
+    echo 'DEPENDENCIES: [gpg, tar]'
+    echo -e 'krypter uses tar to convert files /directories into/from archives, and uses gpg to encrypt/decrypt a file/directory using the AES-256 algorithm \n'
+    echo 'EXAMPLES:'
+    echo '  krypter -e file.txt'
+    echo -e '  krypter -dr file.gpg \n'
+    echo 'OPTIONS:'
+    echo '  -e         encrypt file/directory'
+    echo '  -d         decrypt file'
+    echo -e '  -r         delete the original file on success \n'
+}
 getPassHash() {
-    local PASS
-    read -sp "Password: " PASS
-    local HASH="$(echo $PASS | sha256sum)"
-    HASHED_PASS=$(printf '%s\n' "${HASH%%" -"}") #remove trailing " -" from hash
+    local KEY
+    read -sp "Secret key: `echo $'\n> '`" KEY
+    local HASH="$(echo $KEY | sha256sum)"
+    HASHED_KEY=$(printf '%s\n' "${HASH%%" -"}") #remove trailing " -" from hash
 }
 decrypter() {
     getPassHash
     local NAME="${2%%.*}"
-    $(gpg --output $NAME.tar.gz --batch --decrypt --passphrase $HASHED_PASS $2)
-    $(mkdir $NAME/)
-    $(tar xzf $NAME.tar.gz -C $NAME/)
-    $(rm $NAME.tar.gz)
+    $(gpg --output $NAME.tar.gz --batch --decrypt --passphrase $HASHED_KEY $2)
+    if [[ -f "$NAME.tar.gz" ]]
+    then
+        echo -e '\033[1mDecryption successful!\033[0m'
+        $(mkdir $NAME/)
+        $(tar xzf $NAME.tar.gz -C $NAME/)
+        $(rm $NAME.tar.gz)
+        SUCCESS=1
+    elif [[ ! -f "NAME.tar.gz" ]]
+    then
+        echo -e '\033[1mDECRYPTION FAILED!\033[0m'
+        echo 'Perhaps you entered the wrong key. Please try again'
+   fi
 }
+FILE=$2
 encrypter() {
-    if [[ -d "$2" ]]
-    then
-        local NAME="$(basename $2)"
-        $(tar czf $NAME.tar.gz --directory=$NAME/ .)
+    local NAME="$(echo $(basename -a "$FILE") | tr " " - )"
 
-    elif [[ -f "$2" ]]
-    then
-        local NAME="${2%%.*}"
-        $(tar czf $NAME.tar.gz $2)
+    if [[ -d $FILE ]]; then
+        $(tar czf $NAME.tar.gz --directory="$FILE" .)
+    elif [[ -f $FILE ]]; then
+        NAME=${NAME%%.*}
+        $(tar czf $NAME.tar.gz "$2")
+
+    fi
+    if [[ -f $NAME.gpg ]]; then 
+        echo 'An encrypted archive with that name already exists'
+        exit
     fi
 
     getPassHash
-    echo ""
-    $(gpg --cipher-algo AES256 --batch --symmetric --passphrase $HASHED_PASS $NAME.tar.gz)
-    $(rm $NAME.tar.gz)
-}
-
-if [[ $1 == "--decrypt" || $1 == "-d" || $1 == "decrypt" ]]
-then
-    decrypter $1 $2
-
-elif [[ $1 == "--encrypt" || $1 == "-e" || $1 == "encrypt" ]]
-then
-    encrypter $1 $2
-fi
-
-deleter() {
-    if [[ $1 == "--rm-original" || $1 == "-r" ]]
-    then
-        $(rm $2)
+    if [[ -f "$NAME.tar.gz" ]]; then
+        $(gpg --cipher-algo AES256 --batch --symmetric --output $NAME.gpg --passphrase $HASHED_KEY $NAME.tar.gz)
+        $(rm $NAME.tar.gz)
+    fi
+    if [[ -f "$NAME.gpg" ]]; then
+        echo -e '\033[1mEncryption successful!\033[0m'
+        SUCCESS=1
     fi
 }
 
-if [[ -n "$3" ]]
-then
-    deleter $3 $2
+
+# ARGUMENT HANDLING ############################################################################
+if [[ $# == 0 ]]; then
+    echo 'krypter: You must specify which mode you want to run and which file or directory you want to target'
+    echo "Try 'krypter -h' for more information"
+elif [[ $# == 1 && $1 != "-h" ]]; then
+    echo 'krypter: Two arguments are required, a mode and a file or directory'
+    echo "Try 'krypter -h' for more information"
+elif [[ $# > 2 ]]; then
+    echo 'krypter: Too many arguments'
+    echo "Try 'krypter -h' for more information"
+fi
+# argument parser
+while getopts 'hred' OPTION; do
+    case $OPTION in
+        h) # print help
+            HELP
+            exit;;
+        r) # delete orignal file on process completion
+            DELETE_MODE=1;;
+        e) # encrypt file
+            MODE=0;;
+        d) # decrypt file
+            MODE=1;;
+        \? )
+            echo 'Error. -'$OPTARG' Invalid option'
+            exit;;
+    esac
+done
+
+# function triggers
+if [[ $MODE == 0 ]]; then
+    encrypter $@
+elif [[ $MODE == 1 ]]; then
+    decrypter "$@"
+fi
+
+# DELETE CHECKER AND TRIGGER
+#
+if [[ $DELETE_MODE == 1 && $SUCCESS == 1 ]]; then
+    $(rm -rf $2)
 fi
